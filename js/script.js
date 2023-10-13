4457e69210cfc0fe19c103a166bcf7b62f69d5dd
ben64103 <benabolade@gmail.com>
2023-10-13 09:22:09 +0100

    Added details such as hotel names, star ratings, qualities, features, descriptions, and addresses for 1

--- js/script.js
+++
@@ -1,8 +0,0 @@
-import { functionName } from "./pages/places.js";
-import { serverLink } from "./pages/places.js";
-
-const server = {server: 'server1', request: 'requesting data from database'}
-
-functionName()
-serverLink(server)
-
