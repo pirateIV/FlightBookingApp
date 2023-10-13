01fd962cea320b911f0950889428fcbedfe8b502
ben64103 <benabolade@gmail.com>
2023-10-13 07:19:38 +0100

    header done

---
+++ js/script.js
@@ -0,0 +1,8 @@
+import { functionName } from "./pages/places.js";
+import { serverLink } from "./pages/places.js";
+
+const server = {server: 'server1', request: 'requesting data from database'}
+
+functionName()
+serverLink(server)
+
